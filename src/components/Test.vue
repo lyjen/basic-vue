2fbd9e4a14672830e09fd5bdca9588cea32aed1b
Lyjen Lovendino <lyjen_ljelaine049@yahoo.com>
2019-02-25 16:43:29 +0800

    Add files via upload

---
+++ src/components/Test.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="test2">
+	<input type="text" v-model="title"/>
+	<h1>{{title}}</h1>
+	<p v-if="showName">- {{user.first_name}}</p>
+	<p v-else> </p><br />
+	<p v-for="item in items">{{item.title}}</p>
+	<button v-on:click="greet('I miss you')">Try Me</button><br />
+	<input type="text" v-on:keyup="pressKey" v-on:keyup.enter="hitEnter"/>
+	<br />
+	<label>First Name:</label> <input type="text" v-model="user.first_name" />
+	<br />
+	<label>Last Name:</label> <input type="text" v-model="user.last_name" />
+	<h3>{{fullName}}</h3>
+	<p>{{msg}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+	msg: {
+		type: String,
+		default: 'Message'
+	}
+  },
+  data(){
+	return{
+		title: ' ',
+		user:{
+			first_name: 'LJ',
+			last_name: 'Lovendino'
+		},
+		showName: false,
+		items:[
+			{title: 'Plan1'},
+			{title: 'Plan2'},
+			{title: 'Plan3'},
+		]
+		}	
+	},
+	methods: {
+		greet: function(greeting){
+			alert(greeting);
+		},
+		pressKey: function(e){
+		console.log(e.target.value);
+		},
+		hitEnter: function(){
+		console.log('You hit enter');
+		}
+		
+	},
+	computed: {
+		fullName: function(){
+			return this.user.first_name+ ' '+this.user.last_name;
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="users">
+		<h1>Users</h1>
+		<form v-on:submit="addUser">
+			<input type="text" v-model="newUser.name" placeholder="Enter name"/>
+			<br />
+			<input type="text" v-model="newUser.email" placeholder="Enter email"/>
+			<br />
+			<input type="submit" value="Submit"/>
+		</form>
+		<ul> 
+			<li v-for="user in users">
+			<input type="checkbox" class="toggle" v-model="user.activated"/>
+			<span :class="{activated: user.activated}">{{user.name}} : {{user.email}}
+			<button v-on:click="deleteUser(user)">x</button>
+			</span>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+
+  data(){
+	return{
+		newUser:{},
+		users:[
+		{
+			name: 'LJ',
+			email: 'mslyjen.lyj@email.com',
+			activated: true
+		},
+		{
+			name: 'Oliver',
+			email: 'oliver@email.com',
+			activated: true
+		},
+		{
+			name: 'Chito',
+			email: 'chito@email.com',
+			activated: false
+		}
+		]
+		}	
+	},
+	methods: {
+		addUser: function(e){
+			this.users.push({
+			name: this.newUser.name,
+			email: this.newUser.email,
+			activated: false
+			
+			});
+			e.preventDefault();
+		},
+		deleteUser: function(user){
+			this.users.splice(this.users.indexOf(user),1);
+		}
+		
+	},
+	computed: {
+		
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.users{
+width:300px;
+margin:0 auto;
+padding: 0;
+
+}
+ul li{
+list-style: none;
+text-align: left;
+}
+
+.activated{
+text-decoration: line-through;
+}
+</style>
